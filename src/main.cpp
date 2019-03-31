a4681321040ca175a6b12e8d78b0459e63315ed7
Cristian Arrieta <cristianisaac94@gmail.com>
2019-03-31 14:22:28 -0500

    Cleaning test

--- src/main.cpp
+++ src/main.cpp
@@ -1,22 +1,51 @@
 #include "ESP8266WiFi.h"
-
+#include "painlessMesh.h"
 #include "DataManager.h"
 
+#define MESH_PREFIX "whateverYouLike"
+#define MESH_PASSWORD "somethingSneaky"
+#define MESH_PORT 5555
+
+Scheduler userScheduler; // to control your personal task
+painlessMesh mesh;
 DataManager *m_dataManager;
+
+// Prototype
+void receivedCallback(uint32_t from, String &msg);
+
+size_t logServerId = 0;
+
+// Send message to the logServer every 10 seconds
+Task myLoggingTask(10000, TASK_FOREVER, []() {
+  if (logServerId == 0) // If we don't know the logServer yet
+    mesh.sendBroadcast(m_dataManager->getJSON());
+  else
+    mesh.sendSingle(logServerId, str);
+});
 
 void setup() {
   Serial.begin(115200);
-  delay(2000);
 
   m_dataManager = new DataManager();
+
+  mesh.setDebugMsgTypes(
+      ERROR | STARTUP |
+      CONNECTION); // set before init() so that you can see startup messages
+
+  mesh.init(MESH_PREFIX, MESH_PASSWORD, &userScheduler, MESH_PORT, WIFI_AP_STA,
+            6);
+  mesh.onReceive(&receivedCallback);
+
+  // Add the task to the your scheduler
+  userScheduler.addTask(myLoggingTask);
+  myLoggingTask.enable();
 }
 
 void loop() {
+  userScheduler.execute(); // it will run mesh scheduler as well
+  mesh.update();
+}
 
-  delay(10);
-  // Serial.println(m_dataManager->getJSON());
-  m_dataManager->getJSON();
-  Serial.println("FREE RAM:" + String(ESP.getFreeHeap()));
-
-  delay(10);
+void receivedCallback(uint32_t from, String &msg) {
+  Serial.printf("logClient: Received from %u msg=%s\n", from, msg.c_str());
 }
