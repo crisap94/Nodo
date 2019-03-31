bb6ca2a7eb68d54b5042d0be8fe06d5f65c3cacc
Cristian Arrieta <cristianisaac94@gmail.com>
2019-03-31 15:37:46 -0500

    Detection LogServer

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 #include "ESP8266WiFi.h"
 #include "painlessMesh.h"
 #include "DataManager.h"
+#include "ArduinoJson.h"
 
 #define MESH_PREFIX "smava"
 #define MESH_PASSWORD "smava1234"
@@ -17,6 +18,8 @@ size_t logServerId = 0;
 
 // Send message to the logServer every 10 seconds
 Task myLoggingTask(5000, TASK_FOREVER, []() {
+
+  
   const char* payload = m_dataManager->getJSON();
   String json = String(payload);
   Serial.println(json);
@@ -51,4 +54,20 @@ void loop() {
 
 void receivedCallback(uint32_t from, String &msg) {
   Serial.printf("logClient: Received from %u msg=%s\n", from, msg.c_str());
+
+  const size_t capacity = JSON_OBJECT_SIZE(2) + 30;
+  DynamicJsonDocument doc(capacity);
+
+  deserializeJson(doc, msg);
+
+  if (doc.containsKey("topic"))
+  {
+    if (String("logServer").equals(doc["topic"].as<String>()))
+    {
+      // check for on: true or false
+      logServerId = doc["nodeId"];
+      Serial.printf("logServer detected!!!\n");
+    }
+    Serial.printf("Handled from %u msg=%s\n", from, msg.c_str());
+  }
 }
