346199c30b9c8ab7435755f8a85b0e8734296354
Cristian Arrieta <cristianisaac94@gmail.com>
2019-03-31 20:20:22 -0500

    Configured

--- lib/Sensors/NTC.cpp
+++ lib/Sensors/NTC.cpp
@@ -5,19 +5,79 @@
 //  Original author: Cristian Arrieta P
 ///////////////////////////////////////////////////////////
 
+/**
+        The class implements a set of methods of the NTC_Thermistor.h
+        interface for working with a NTC thermistor and reading
+        a temperature in Celsius, Fahrenheit and Kelvin.
+
+        https://github.com/YuriiSalimov/NTC_Thermistor
+
+        Created by Yurii Salimov, February, 2018.
+        Released into the public domain.
+*/
+
 #include "NTC.h"
 #include "ESP8266WiFi.h"
 
 NTC *NTC::m_ntc = NULL;
 
 NTC::NTC() {
+  this->pin = PCF8591::PIN::BATT_TEMPERATURE_VOLTAJE;
+  this->referenceResistance = 8000;
+  this->nominalResistance = 100000;
+  this->nominalTemperature = 25;
+  this->bValue = 3950;
   this->m_pcf8591 = PCF8591::getInstance();
-  this->pinConnection = PCF8591::PIN::BATT_TEMPERATURE_VOLTAJE;
-  this->samplingInterval = 5;
+  this->readingsNumber = 10;
+}
+
+double NTC::readCelsius() { return kelvinsToCelsius(readKelvin()); }
+
+double NTC::readFahrenheit() { return kelvinsToFahrenheit(readKelvin()); }
+
+double NTC::readFarenheit() { return readFahrenheit(); }
+
+double NTC::readKelvin() { return resistanceToKelvins(readResistance()); }
+
+inline double NTC::resistanceToKelvins(const double resistance) {
+  const double inverseKelvin =
+      1.0 / this->nominalTemperature +
+      1.0 / this->bValue * log(resistance / this->nominalResistance);
+  return (1.0 / (inverseKelvin));
+}
+
+inline double NTC::readResistance() {
+  return this->referenceResistance / (NTC_ADC / readVoltage() - 1);
+}
+
+inline double NTC::readVoltage() {
+  return this->m_pcf8591->getValue((PCF8591::PIN )this->pin);
+}
+
+void NTC::setReadingsNumber(const int newReadingsNumber) {
+  this->readingsNumber =
+      validate(newReadingsNumber, NTC_DEFAULT_READINGS_NUMBER);
+}
+
+template <typename A, typename B>
+A NTC::validate(const A data, const B alternative) {
+  return (data > 0) ? data : alternative;
+}
+
+inline double NTC::celsiusToKelvins(const double celsius) {
+  return (celsius + 273.15);
+}
+
+inline double NTC::kelvinsToCelsius(const double kelvins) {
+  return (kelvins - 273.15);
+}
+
+inline double NTC::celsiusToFahrenheit(const double celsius) {
+  return (celsius * 1.8 + 32);
 }
 
 NTC::~NTC() {}
 
 float NTC::getValue() {
-  return this->m_pcf8591->getValue((PCF8591::PIN)this->pinConnection);
+  return this->m_ntc->readCelsius();
 }
--- lib/Sensors/NTC.h
+++ lib/Sensors/NTC.h
@@ -5,25 +5,73 @@
 //  Original author: Cristian Arrieta P
 ///////////////////////////////////////////////////////////
 
+/**
+        The class implements a set of methods of the NTC_Thermistor.h
+        interface for working with a NTC thermistor and reading
+        a temperature in Celsius, Fahrenheit and Kelvin.
+
+        https://github.com/YuriiSalimov/NTC_Thermistor
+
+        Created by Yurii Salimov, February, 2018.
+        Released into the public domain.
+*/
+
 #if !defined(EA_CE9233F3_0BB0_419e_A745_11B236F4F9B4__INCLUDED_)
 #define EA_CE9233F3_0BB0_419e_A745_11B236F4F9B4__INCLUDED_
 
-#include "Analog.h"
 #include "Sensor.h"
 #include "pcf8591.h"
 
+/**
+        Min value of Arduino ADC.
+*/
+#define NTC_ADC_MIN 0
 
-class NTC : public Analog, public Sensor {
+/**
+        Max value of Arduino ADC.
+*/
+#define NTC_ADC_MAX 1023
+
+/**
+        Values diapason of Arduino ADC
+*/
+#define NTC_ADC ((NTC_ADC_MAX) - (NTC_ADC_MIN))
+
+#define NTC_DEFAULT_READINGS_NUMBER 5
+#define NTC_DEFAULT_DELAY_TIME 10
+
+class NTC : public Sensor {
 private:
   static NTC *m_ntc;
 
   PCF8591 *m_pcf8591;
 
-  int pinConnection;
-  int samplingInterval;
+  int pin; // an analog port.
+  double referenceResistance;
+  double nominalResistance;
+  double nominalTemperature; // in Celsius.
+  double bValue;
+  int readingsNumber;
 
   NTC();
+
   virtual ~NTC();
+  
+  inline double readResistance();
+
+  inline double readVoltage();
+
+  inline double resistanceToKelvins(double resistance);
+
+  inline double celsiusToKelvins(double celsius);
+
+  inline double kelvinsToCelsius(double kelvins);
+
+  inline double celsiusToFahrenheit(double celsius);
+
+  inline double kelvinsToFahrenheit(double kelvins);
+
+  template <typename A, typename B> A validate(A data, B alternative);
 
 public:
   static NTC *getInstance() {
@@ -32,6 +80,18 @@ public:
     }
     return m_ntc;
   }
+
   float getValue();
+
+  double readCelsius();
+
+  double readKelvin();
+
+  double readFahrenheit();
+
+  double readFarenheit();
+
+  void setReadingsNumber(int newReadingsNumber);
+
 };
 #endif // !defined(EA_CE9233F3_0BB0_419e_A745_11B236F4F9B4__INCLUDED_)
