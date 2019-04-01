0677a307fd12a7ee12bd4b58bc1b829e62ac8bae
Cristian Arrieta <cristianisaac94@gmail.com>
2019-03-31 23:19:25 -0500

    Configured

--- lib/Sensors/DS18B20.cpp
+++ lib/Sensors/DS18B20.cpp
@@ -5,22 +5,27 @@
 //  Original author: Cristian Arrieta P
 ///////////////////////////////////////////////////////////
 
+#include "OneWire.h"
+
 #include "DS18B20.h"
+
 #include "ESP8266WiFi.h"
+
+#include <DallasTemperature.h>
 
 DS18B20 *DS18B20::m_ds18b20 = NULL;
 
-DS18B20::DS18B20() { randomSeed(analogRead(A0)); }
+DS18B20::DS18B20() {
+  this->oneWire = new OneWire(ONE_WIRE_BUS);
+  this->ds18b20 = new DallasTemperature(this->oneWire);
 
-DS18B20::~DS18B20(){
-
+  this->ds18b20->begin();
 }
 
-void DS18B20::begin(){
+DS18B20::~DS18B20() {}
 
-}
-
-
-float DS18B20::getValue(){
-	return random(25,27);
+float DS18B20::getValue() {
+  this->ds18b20->requestTemperatures();
+  float temp = this->ds18b20->getTempCByIndex(0);
+  return temp;
 }
--- lib/Sensors/DS18B20.h
+++ lib/Sensors/DS18B20.h
@@ -9,12 +9,27 @@
 #define EA_2CE8C70C_CC43_4bab_A992_A12AA34F908F__INCLUDED_
 
 #include "OneWire.h"
+
 #include "Sensor.h"
 
-class DS18B20 : public Sensor, public OneWire {
+#include <DallasTemperature.h>
+
+
+// Data wire is plugged into port 2 on the Arduino
+#define ONE_WIRE_BUS 2
+
+
+
+class DS18B20 : public Sensor{
 private:
   static DS18B20 *m_ds18b20;
 
+  // Setup a oneWire instance to communicate with any OneWire devices (not just
+  // Maxim/Dallas temperature ICs)
+  OneWire *oneWire;
+
+  // Pass our oneWire reference to Dallas Temperature.
+  DallasTemperature *ds18b20;
   DS18B20();
   virtual ~DS18B20();
 
@@ -25,7 +40,7 @@ public:
     }
     return m_ds18b20;
   }
-  void begin();
+
   float getValue();
 };
 #endif // !defined(EA_2CE8C70C_CC43_4bab_A992_A12AA34F908F__INCLUDED_)
