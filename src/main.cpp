03555aafaa082406c30304eac4182fe45bfc052d
Cristian Arrieta <cristianisaac94@gmail.com>
2019-05-16 13:27:00 -0500

    new main

--- src/main.cpp
+++ src/main.cpp
@@ -1,82 +1,15 @@
-#include "ESP8266WiFi.h"
+#include <ESP8266WiFi.h>
 
-#include "painlessMesh.h"
+#include <ConectionManager.h>
 
-#include "DataManager.h"
-
-#include "ArduinoJson.h"
-
-#define MESH_PREFIX "smava"
-#define MESH_PASSWORD "smava1234"
-#define MESH_PORT 5555
-
-Scheduler userScheduler; // to control your personal task
-painlessMesh mesh;
-DataManager *m_dataManager;
-
-// Prototype
-void receivedCallback(uint32_t from, String &msg);
-
-size_t logServerId = 0;
-
-// Send message to the logServer every 10 seconds
-Task myLoggingTask(TASK_SECOND * 5, TASK_FOREVER, []() {
-  /*   String json = m_dataManager->getJSON();
-
-    Serial.println(json); */
-
-  String broadcast = "BROADCASTINGGGGGG";
-
-  String srvr = "{\"test\":\"Prueba\"}";
-  if (logServerId == 0) // If we don't know the logServer yet
-    mesh.sendBroadcast(broadcast);
-  else
-    mesh.sendSingle(logServerId, srvr);
-});
+ConectionManager *m_conectionManager = new ConectionManager();
 
 void setup() {
   Serial.begin(115200);
-
-  m_dataManager = new DataManager();
-
-  // mesh.setDebugMsgTypes(ERROR | MESH_STATUS | CONNECTION | SYNC |
-  // COMMUNICATION /* | GENERAL */ | MSG_TYPES | REMOTE); // set before init()
-  // so that you can see startup messages
-  mesh.setDebugMsgTypes(
-      ERROR | MESH_STATUS | CONNECTION | SYNC | COMMUNICATION /* | GENERAL */ |
-      MSG_TYPES |
-      REMOTE); // set before init() so that you can see startup messages
-
-  mesh.init(MESH_PREFIX, MESH_PASSWORD, &userScheduler, MESH_PORT, WIFI_AP_STA,
-            6);
-  mesh.onReceive(&receivedCallback);
-
-  mesh.setRoot(false);
-
-  // Add the task to the your scheduler
-  userScheduler.addTask(myLoggingTask);
-  myLoggingTask.enable();
+  m_conectionManager->begin();
 }
 
 void loop() {
-  userScheduler.execute(); // it will run mesh scheduler as well
-  mesh.update();
+  m_conectionManager->loop();
 }
 
-void receivedCallback(uint32_t from, String &msg) {
-  Serial.printf("logClient: Received from %u msg=%s\n", from, msg.c_str());
-
-  const size_t capacity = JSON_OBJECT_SIZE(2) + 30;
-  DynamicJsonDocument doc(capacity);
-
-  deserializeJson(doc, msg);
-
-  if (doc.containsKey("topic")) {
-    if (String("logServer").equals(doc["topic"].as<String>())) {
-      // check for on: true or false
-      logServerId = doc["nodeId"];
-      Serial.printf("logServer detected!!!\n");
-    }
-    Serial.printf("Handled from %u msg=%s\n", from, msg.c_str());
-  }
-}
