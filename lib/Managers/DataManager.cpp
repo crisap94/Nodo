6079b8cb2e8e2aceb83f11c71da08f9cfd9f9671
Cristian Arrieta <cristianisaac94@gmail.com>
2019-05-18 16:26:00 -0500

    Interval for Task
    Implemented the time for each sensor

--- lib/Managers/DataManager.cpp
+++ lib/Managers/DataManager.cpp
@@ -6,82 +6,203 @@
 ///////////////////////////////////////////////////////////
 
 #include "DataManager.h"
-#include "ESP8266WiFi.h"
+
 #include "ArduinoJson.h"
-#include "FactorySensorManager.h"
 
 DataManager::DataManager()
-    : managers{
-          FactorySensorManager::TYPE_SENSOR_MANAGER::AMBIENT_HUMIDITY,
-          FactorySensorManager::TYPE_SENSOR_MANAGER::AMBIENT_TEMPERATURE,
-          FactorySensorManager::TYPE_SENSOR_MANAGER::AMBIENT_PRESSURE,
-          FactorySensorManager::TYPE_SENSOR_MANAGER::SOIL_TEMP,
-          FactorySensorManager::TYPE_SENSOR_MANAGER::SOIL_HUMIDITY,
-          FactorySensorManager::TYPE_SENSOR_MANAGER::BATT_VOLTAJE,
-          FactorySensorManager::TYPE_SENSOR_MANAGER::BATT_TEMPERATURE,
-          FactorySensorManager::TYPE_SENSOR_MANAGER::BATT_STATUS,
-          FactorySensorManager::TYPE_SENSOR_MANAGER::PANEL_VOLTAJE,
-          FactorySensorManager::TYPE_SENSOR_MANAGER::ULTRAVIOLE_INDEX,
-          FactorySensorManager::TYPE_SENSOR_MANAGER::BRIGHTNESS,
-          FactorySensorManager::TYPE_SENSOR_MANAGER::VOLATILE_ORGANIC_COMPUNDS,
-          FactorySensorManager::TYPE_SENSOR_MANAGER::EPOCH} {
-  this->m_factoryManager = new FactorySensorManager();
+
+    : managers{ManagerType::AMBIENT_HUMIDITY,
+               ManagerType::AMBIENT_TEMPERATURE,
+               ManagerType::AMBIENT_PRESSURE,
+               ManagerType::SOIL_TEMP,
+               ManagerType::SOIL_HUMIDITY,
+               ManagerType::BATT_VOLTAJE,
+               ManagerType::BATT_TEMPERATURE,
+               ManagerType::BATT_STATUS,
+               ManagerType::PANEL_VOLTAJE,
+               ManagerType::ULTRAVIOLE_INDEX,
+               ManagerType::BRIGHTNESS,
+               ManagerType::VOLATILE_ORGANIC_COMPUNDS,
+               ManagerType::EPOCH},
+      intervals{Interval::INTERVAL_AMBIENT_HUMIDITY,
+                Interval::INTERVAL_AMBIENT_PRESSURE,
+                Interval::INTERVAL_AMBIENT_PRESSURE,
+                Interval::INTERVAL_SOIL_TEMP,
+                Interval::INTERVAL_SOIL_HUMIDITY,
+                Interval::INTERVAL_BATT_VOLTAJE,
+                Interval::INTERVAL_BATT_TEMPERATURE,
+                Interval::INTERVAL_BATT_STATUS,
+                Interval::INTERVAL_PANEL_VOLTAJE,
+                Interval::INTERVAL_ULTRAVIOLE_INDEX,
+                Interval::INTERVAL_BRIGHTNESS,
+                Interval::INTERVAL_VOLATILE_ORGANIC_COMPUNDS,
+                Interval::INTERVAL_EPOCH},
+      algorithm{Algorithm::ALGORITHM_AMBIENT_HUMIDITY,
+                Algorithm::ALGORITHM_AMBIENT_PRESSURE,
+                Algorithm::ALGORITHM_AMBIENT_PRESSURE,
+                Algorithm::ALGORITHM_SOIL_TEMP,
+                Algorithm::ALGORITHM_SOIL_HUMIDITY,
+                Algorithm::ALGORITHM_BATT_VOLTAJE,
+                Algorithm::ALGORITHM_BATT_TEMPERATURE,
+                Algorithm::ALGORITHM_BATT_STATUS,
+                Algorithm::ALGORITHM_PANEL_VOLTAJE,
+                Algorithm::ALGORITHM_ULTRAVIOLE_INDEX,
+                Algorithm::ALGORITHM_BRIGHTNESS,
+                Algorithm::ALGORITHM_VOLATILE_ORGANIC_COMPUNDS,
+                Algorithm::ALGORITHM_EPOCH} {
+
+  Serial.println("DEBUG -> Starting Factory Manager");
+
+  m_factoryManager = new FactorySensorManager();
+
+  Serial.println("DEBUG -> Setting to NOT_SET all the Variables");
   for (size_t manager = 0; manager < MANAGER_SIZE; manager++) {
-    this->variables[manager] = -1;
+    variables[manager] = NOT_SET;
+  }
+
+  Serial.println("DEBUG -> Setting the matrix of Raw Values to NOT_SET");
+  for (size_t managerData = 0; managerData < MANAGER_SIZE; managerData++) {
+    for (size_t sample = 0; sample < SAMPLES; sample++) {
+      rawValues[managerData][sample] = NOT_SET;
+    }
+  }
+
+  Serial.println("DEBUG -> Creating Scheduller");
+  m_dataScheduler = new Scheduler();
+
+  for (size_t task = 0; task < MANAGER_SIZE; task++) {
+    Serial.printf("DEBUG -> Creating Task (%s)\n", MANAGER_TYPE(task).c_str());
+    m_tasks[task] =
+        new Task(TASK_MILLISECOND * intervals[task], TASK_FOREVER,
+                 [this, task]() {
+
+                   uint8_t dataset = task;
+
+                   /**
+                    * @brief Verify if the corresponding dataset of values has
+                    * any missing sample like NOT_SET, which mean the value has
+                    * not been taken.
+                    */
+                   for (size_t sample = 0; sample < SAMPLES; sample++) {
+                     if (rawValues[dataset][sample] == NOT_SET) {
+                       /**
+                        * @brief once the sample is found, create the
+                        * corresponding manager to get only 1 value
+                        * that corresponds to the dataset
+                        *
+                        */
+
+                       ManagerType type = (ManagerType)task;
+                       SensorManager *manager;
+                       manager = m_factoryManager->createManager(type);
+                       this->rawValues[dataset][sample] = manager->getData();
+
+                       Serial.printf("Gettin Value from (%s) -> \t\t\t(%d)\n",
+                                     MANAGER_TYPE(task).c_str(),
+                                     this->rawValues[dataset][sample]);
+
+                       delete manager;
+
+                       break;
+
+                     } else if (sample == (SAMPLES - 1)) {
+
+                       Serial.printf("\n\nDisabling task (%s)\n\n",
+                                     MANAGER_TYPE(task).c_str());
+                       /**
+                        * @brief if all the data are already gathered then
+                        * the task is disabled
+                        *
+                        */
+                       this->m_tasks[task]->disable();
+
+                       /**
+                        * @brief After
+                        *
+                        */
+                       Serial.printf("Updating average value for (%s)\n\n",
+                                     MANAGER_TYPE(task).c_str());
+                       m_cleaner = new DataCleaner<long>;
+                       m_cleaner->begin(algorithm[task], SAMPLES);
+                       for (size_t sample = 0; sample < SAMPLES; sample++) {
+                         m_cleaner->add(rawValues[task][sample]);
+                       }
+                       variables[task] = m_cleaner->get();
+                       delete m_cleaner;
+                     }
+                   }
+                 },
+                 m_dataScheduler);
+  }
+
+  for (size_t task = 0; task < MANAGER_SIZE; task++) {
+    // Enable all the Tasks previusly added to thescheduller
+    m_tasks[task]->enable();
   }
 }
 
 DataManager::~DataManager() {}
 
-void DataManager::getVariableData() {
-  SensorManager *m_sensorManager;
-  for (int manager = 0; manager < MANAGER_SIZE; manager++) {
-    m_sensorManager = m_factoryManager->createManager(this->managers[manager]);
-    m_sensorManager->getCleanedData([&](long data) -> void { variables[manager] = data; });
-    delete m_sensorManager;
+bool DataManager::dataAvailable() {
+  for (size_t dataset = 0; dataset < MANAGER_SIZE; dataset++) {
+    for (size_t sample = 0; sample < SAMPLES; sample++) {
+      if (rawValues[dataset][sample] == NOT_SET) {
+        return false;
+      }
+    }
   }
-    yield();
+
+  return true;
 }
 
-void DataManager::getPayload() {
-  getVariableData();
-
-  const size_t capacity = JSON_OBJECT_SIZE(2) + JSON_OBJECT_SIZE(3) + JSON_OBJECT_SIZE(4) + JSON_OBJECT_SIZE(13);
-  DynamicJsonDocument doc(capacity);
-
-  JsonObject data = doc.createNestedObject("data");
-  data["temperature"]             = this->variables[FactorySensorManager::TYPE_SENSOR_MANAGER::AMBIENT_TEMPERATURE];
-  data["humidity"]                = this->variables[FactorySensorManager::TYPE_SENSOR_MANAGER::AMBIENT_HUMIDITY];
-  data["soilHumidity"]            = this->variables[FactorySensorManager::TYPE_SENSOR_MANAGER::SOIL_HUMIDITY];
-  data["pressure"]                = this->variables[FactorySensorManager::TYPE_SENSOR_MANAGER::AMBIENT_PRESSURE];
-  data["uv"]                      = this->variables[FactorySensorManager::TYPE_SENSOR_MANAGER::ULTRAVIOLE_INDEX];
-  data["brightness"]              = this->variables[FactorySensorManager::TYPE_SENSOR_MANAGER::BRIGHTNESS];
-  data["floorTemperature"]        = this->variables[FactorySensorManager::TYPE_SENSOR_MANAGER::SOIL_TEMP];
-  data["volatileGases"]           = this->variables[FactorySensorManager::TYPE_SENSOR_MANAGER::VOLATILE_ORGANIC_COMPUNDS];
-  data["panelvoltaje"]            = this->variables[FactorySensorManager::TYPE_SENSOR_MANAGER::PANEL_VOLTAJE];
-  data["timestamp"]               = this->variables[FactorySensorManager::TYPE_SENSOR_MANAGER::EPOCH];
-
-  JsonObject data_gps = data.createNestedObject("gps");
-  data_gps["latitude"]            = "10.399080";
-  data_gps["longitude"]           = "-75.504142";
-
-  data["batteryVoltaje"]          = this->variables[FactorySensorManager::TYPE_SENSOR_MANAGER::BATT_VOLTAJE];
-  data["batteryTmeperature"]      = this->variables[FactorySensorManager::TYPE_SENSOR_MANAGER::BATT_TEMPERATURE];
-
-  doc["zoneId"]                   = "5ac012583e194204e0afef6b";
-  doc["firmwareVersion"]          = "V1.0";
-
-  JsonObject config = doc.createNestedObject("config");
-  config["mac"]                   = WiFi.macAddress();
-  config["rssi"]                  = WiFi.RSSI(0);
-  config["channel"]               = WiFi.channel();
-
-  serializeJson(doc, this->json_array);
-
+bool DataManager::isReady() {
+  for (size_t variable = 0; variable < MANAGER_SIZE; variable++) {
+    if (variables[variable] == NOT_SET) {
+      return false;
+    }
+  }
+  return true;
 }
 
-String DataManager::getJSON(){
-  getPayload();
+String DataManager::getPayload() {
 
-  return json_array;
-} 
+  if (isReady()) {
+    const size_t capacity = JSON_OBJECT_SIZE(2) + JSON_OBJECT_SIZE(3) +
+                            JSON_OBJECT_SIZE(4) + JSON_OBJECT_SIZE(13);
+    DynamicJsonDocument doc(capacity);
+
+    doc["t"] = variables[ManagerType::AMBIENT_TEMPERATURE];
+    doc["h"] = variables[ManagerType::AMBIENT_HUMIDITY];
+    doc["sH"] = variables[ManagerType::SOIL_HUMIDITY];
+    doc["p"] = variables[ManagerType::AMBIENT_PRESSURE];
+    doc["u"] = variables[ManagerType::ULTRAVIOLE_INDEX];
+    doc["b"] = variables[ManagerType::BRIGHTNESS];
+    doc["sT"] = variables[ManagerType::SOIL_TEMP];
+    doc["v"] = variables[ManagerType::VOLATILE_ORGANIC_COMPUNDS];
+    doc["pV"] = variables[ManagerType::PANEL_VOLTAJE];
+    doc["ti"] = variables[ManagerType::EPOCH];
+    doc["la"] = "10.399080";
+    doc["lo"] = "-75.504142";
+    doc["bV"] = variables[ManagerType::BATT_VOLTAJE];
+    doc["bT"] = variables[ManagerType::BATT_TEMPERATURE];
+
+    serializeJsonPretty(doc, json_array);
+
+    resetValues();
+
+    return json_array;
+  }
+
+  return "The Payload is not ready yet";
+}
+
+void DataManager::resetValues() {
+  for (size_t dataset = 0; dataset < MANAGER_SIZE; dataset++) {
+    variables[dataset] = NOT_SET;
+    for (size_t sample = 0; sample < SAMPLES; sample++) {
+      rawValues[dataset][sample] = NOT_SET;
+    }
+  }
+}
+
+void DataManager::loop() { m_dataScheduler->execute(); }
--- lib/Managers/DataManager.h
+++ lib/Managers/DataManager.h
@@ -10,21 +10,96 @@
 
 #include "FactorySensorManager.h"
 
+#include "DataCleaner.h"
+
+#define _TASK_STD_FUNCTION
+#define _TASK_INLINE
+#include <TaskScheduler.h>
+
 #define MANAGER_SIZE 13
+#define SAMPLES 10
+
+#define NOT_SET -1
+#define TASK_ENABLED true
+
+#define MANAGER_TYPE(type)                                                     \
+  this->m_factoryManager->getManagerType((ManagerType)type)
 
 class DataManager {
 
 public:
+
+/**
+ * @brief 
+ * TODO Place the Intervals from the Document
+ * 
+ */
+  enum VARIABLE_SENSING_INTERVAL{
+    INTERVAL_AMBIENT_HUMIDITY            = 1500,
+    INTERVAL_AMBIENT_TEMPERATURE         = 1000,
+    INTERVAL_AMBIENT_PRESSURE            = 1000,
+    INTERVAL_SOIL_TEMP                   = 100,
+    INTERVAL_SOIL_HUMIDITY               = 100,
+    INTERVAL_BATT_VOLTAJE                = 100,
+    INTERVAL_BATT_TEMPERATURE            = 100,
+    INTERVAL_BATT_STATUS                 = 10,
+    INTERVAL_PANEL_VOLTAJE               = 100,
+    INTERVAL_ULTRAVIOLE_INDEX            = 50,
+    INTERVAL_BRIGHTNESS                  = 600,
+    INTERVAL_VOLATILE_ORGANIC_COMPUNDS   = 1400,
+    INTERVAL_EPOCH                       = 10,
+  };
+
+  enum SMOOTH_ALGORITHM{
+    ALGORITHM_AMBIENT_HUMIDITY            = SMOOTHED_AVERAGE,
+    ALGORITHM_AMBIENT_TEMPERATURE         = SMOOTHED_AVERAGE,
+    ALGORITHM_AMBIENT_PRESSURE            = SMOOTHED_AVERAGE,
+    ALGORITHM_SOIL_TEMP                   = SMOOTHED_AVERAGE,
+    ALGORITHM_SOIL_HUMIDITY               = SMOOTHED_EXPONENTIAL,
+    ALGORITHM_BATT_VOLTAJE                = SMOOTHED_EXPONENTIAL,
+    ALGORITHM_BATT_TEMPERATURE            = SMOOTHED_EXPONENTIAL,
+    ALGORITHM_BATT_STATUS                 = SMOOTHED_EXPONENTIAL,
+    ALGORITHM_PANEL_VOLTAJE               = SMOOTHED_AVERAGE,
+    ALGORITHM_ULTRAVIOLE_INDEX            = SMOOTHED_AVERAGE,
+    ALGORITHM_BRIGHTNESS                  = SMOOTHED_AVERAGE,
+    ALGORITHM_VOLATILE_ORGANIC_COMPUNDS   = SMOOTHED_EXPONENTIAL,
+    ALGORITHM_EPOCH                       = SMOOTHED_AVERAGE,
+  };
+
+  String getPayload();
+  bool isReady();
+
+  void loop();
+  
   DataManager();
   virtual ~DataManager();
-  String getJSON();
 
 private:
-  void getPayload();
-  String json_array;
-  void getVariableData();
-  long variables[MANAGER_SIZE];
-  FactorySensorManager::TYPE_SENSOR_MANAGER managers[MANAGER_SIZE];
+
+  typedef FactorySensorManager::TYPE_SENSOR_MANAGER ManagerType;
+  typedef VARIABLE_SENSING_INTERVAL Interval;
+  typedef  SMOOTH_ALGORITHM Algorithm;
+
   FactorySensorManager *m_factoryManager;
+  DataCleaner<long> *m_cleaner;
+
+  ManagerType managers[MANAGER_SIZE];
+  uint16_t intervals[MANAGER_SIZE];
+  uint16_t algorithm[MANAGER_SIZE];
+
+
+  Scheduler *m_dataScheduler;
+  Task *m_tasks[MANAGER_SIZE];
+
+  long rawValues[MANAGER_SIZE][SAMPLES];
+
+  long variables[MANAGER_SIZE];
+
+  String json_array;
+
+  bool dataAvailable();
+
+  void resetValues();
+
 };
 #endif // !defined(EA_542022A9_4749_4b6e_BA6F_80DFB9BB0B34__INCLUDED_)
