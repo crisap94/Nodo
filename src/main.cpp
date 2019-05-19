b0993341faffc6fa91e225001de743493ee4e8be
Cristian Arrieta <cristianisaac94@gmail.com>
2019-05-19 16:00:30 -0500

    Adding Soil to Main

--- src/main.cpp
+++ src/main.cpp
@@ -34,18 +34,19 @@ void setup() {
   m_ds32 = DS3231::getInstance();
   m_veml = VEML6075::getInstance();
   m_sol = SolarPanel::getInstance();
+  m_soil = CapacitiveSoilMoisture::getInstance();
 
   // m_dataManager = new DataManager();
 }
 
 void loop() {
 
-  Serial.printf("BattTemp: %2.1f BattVolt: %1.1f PanelVolt: %1.1f Humidity: %2.1f "
-                "Temperature: %2.1f Pressure: %4.1f VOC: %3.1f EPOCH: %lld UV: %1.1f \n",
+  Serial.printf("BattT: %2.1f BattV: %1.1f PanelV: %1.1f Hum: %2.1f "
+                "Temp: %2.1f Press: %4.1f VOC: %3.1f EPOCH: %lld UV: %1.1f SoilH: %3.0f\n",
                 m_batt->getTemp(), m_batt->getVoltaje(), m_sol->getVoltaje(),
                 m_bme->getHumidity(), m_bme->getTemperature(),
                 m_bme->getPressure(), m_bme->getVOC(), m_ds32->getValue(),
-                m_veml->getValue());
+                m_veml->getValue(),m_soil->getValue());
 
   // m_dataManager->loop();
   // if (m_dataManager->isReady()) {
