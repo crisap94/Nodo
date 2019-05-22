3b0ae4ce8df66e55ecab2dda1123ee1d44d53bab
Cristian Arrieta <cristianisaac94@gmail.com>
2019-05-22 00:49:21 -0500

    Organizing File and adding Lat and Lon

--- lib/Managers/DataManager.h
+++ lib/Managers/DataManager.h
@@ -13,9 +13,10 @@
 
 #include "DataCleaner.h"
 
+#define _TASK_SLEEP_ON_IDLE_RUN
+#define _TASK_STATUS_REQUEST
 #define _TASK_STD_FUNCTION
-#define _TASK_INLINE
-#include <TaskScheduler.h>
+#include <TaskSchedulerDeclarations.h>
 
 #define MANAGER_SIZE 11
 #define SAMPLES 10
@@ -29,73 +30,72 @@
 class DataManager {
 
 public:
-
-/**
- * @brief 
- * TODO Place the Intervals from the Document
- * 
- */
-  enum VARIABLE_SENSING_INTERVAL{
-    INTERVAL_AMBIENT_HUMIDITY            = 1500,
-    INTERVAL_AMBIENT_TEMPERATURE         = 1000,
-    INTERVAL_AMBIENT_PRESSURE            = 1000,
-    INTERVAL_SOIL_TEMP                   = 100,
-    INTERVAL_SOIL_HUMIDITY               = 100,
-    INTERVAL_BATT_VOLTAJE                = 100,
-    INTERVAL_BATT_TEMPERATURE            = 100,
-    //INTERVAL_BATT_STATUS                 = 10,
-    INTERVAL_PANEL_VOLTAJE               = 100,
-    INTERVAL_ULTRAVIOLE_INDEX            = 50,
-    INTERVAL_BRIGHTNESS                  = 600,
-    INTERVAL_VOLATILE_ORGANIC_COMPUNDS   = 1400,
-    //INTERVAL_EPOCH                       = 10,
+  /**
+   * @brief
+   * TODO Place the Intervals from the Document
+   *
+   */
+  enum VARIABLE_SENSING_INTERVAL {
+    INTERVAL_AMBIENT_HUMIDITY = 1500,
+    INTERVAL_AMBIENT_TEMPERATURE = 1000,
+    INTERVAL_AMBIENT_PRESSURE = 1000,
+    INTERVAL_SOIL_TEMP = 100,
+    INTERVAL_SOIL_HUMIDITY = 100,
+    INTERVAL_BATT_VOLTAJE = 100,
+    INTERVAL_BATT_TEMPERATURE = 100,
+    // INTERVAL_BATT_STATUS                 = 10,
+    INTERVAL_PANEL_VOLTAJE = 100,
+    INTERVAL_ULTRAVIOLE_INDEX = 50,
+    INTERVAL_BRIGHTNESS = 600,
+    INTERVAL_VOLATILE_ORGANIC_COMPUNDS = 1400,
+    // INTERVAL_EPOCH                       = 10,
   };
 
-  enum SMOOTH_ALGORITHM{
-    ALGORITHM_AMBIENT_HUMIDITY            = SMOOTHED_AVERAGE,
-    ALGORITHM_AMBIENT_TEMPERATURE         = SMOOTHED_AVERAGE,
-    ALGORITHM_AMBIENT_PRESSURE            = SMOOTHED_AVERAGE,
-    ALGORITHM_SOIL_TEMP                   = SMOOTHED_EXPONENTIAL,
-    ALGORITHM_SOIL_HUMIDITY               = SMOOTHED_EXPONENTIAL,
-    ALGORITHM_BATT_VOLTAJE                = SMOOTHED_EXPONENTIAL,
-    ALGORITHM_BATT_TEMPERATURE            = SMOOTHED_EXPONENTIAL,
-    //ALGORITHM_BATT_STATUS                 = SMOOTHED_EXPONENTIAL,
-    ALGORITHM_PANEL_VOLTAJE               = SMOOTHED_AVERAGE,
-    ALGORITHM_ULTRAVIOLE_INDEX            = SMOOTHED_AVERAGE,
-    ALGORITHM_BRIGHTNESS                  = SMOOTHED_AVERAGE,
-    ALGORITHM_VOLATILE_ORGANIC_COMPUNDS   = SMOOTHED_AVERAGE,
-    //ALGORITHM_EPOCH                       = SMOOTHED_AVERAGE,
+  enum SMOOTH_ALGORITHM {
+    ALGORITHM_AMBIENT_HUMIDITY = SMOOTHED_AVERAGE,
+    ALGORITHM_AMBIENT_TEMPERATURE = SMOOTHED_AVERAGE,
+    ALGORITHM_AMBIENT_PRESSURE = SMOOTHED_AVERAGE,
+    ALGORITHM_SOIL_TEMP = SMOOTHED_EXPONENTIAL,
+    ALGORITHM_SOIL_HUMIDITY = SMOOTHED_EXPONENTIAL,
+    ALGORITHM_BATT_VOLTAJE = SMOOTHED_EXPONENTIAL,
+    ALGORITHM_BATT_TEMPERATURE = SMOOTHED_EXPONENTIAL,
+    // ALGORITHM_BATT_STATUS                 = SMOOTHED_EXPONENTIAL,
+    ALGORITHM_PANEL_VOLTAJE = SMOOTHED_AVERAGE,
+    ALGORITHM_ULTRAVIOLE_INDEX = SMOOTHED_AVERAGE,
+    ALGORITHM_BRIGHTNESS = SMOOTHED_AVERAGE,
+    ALGORITHM_VOLATILE_ORGANIC_COMPUNDS = SMOOTHED_AVERAGE,
+    // ALGORITHM_EPOCH                       = SMOOTHED_AVERAGE,
   };
 
-  enum VARIABLES{
-    VAR_AMBIENT_HUMIDITY            = 0,
-    VAR_AMBIENT_TEMPERATURE         ,
-    VAR_AMBIENT_PRESSURE            ,
-    VAR_SOIL_TEMP                   ,
-    VAR_SOIL_HUMIDITY               ,
-    VAR_BATT_VOLTAJE                ,
-    VAR_BATT_TEMPERATURE            ,
-    //VAR_BATT_STATUS                , 
-    VAR_PANEL_VOLTAJE               ,
-    VAR_ULTRAVIOLE_INDEX            ,
-    VAR_BRIGHTNESS                  ,
-    VAR_VOLATILE_ORGANIC_COMPUNDS   ,
-    //VAR_EPOCH                       
+  enum VARIABLES {
+    VAR_AMBIENT_HUMIDITY = 0,
+    VAR_AMBIENT_TEMPERATURE,
+    VAR_AMBIENT_PRESSURE,
+    VAR_SOIL_TEMP,
+    VAR_SOIL_HUMIDITY,
+    VAR_BATT_VOLTAJE,
+    VAR_BATT_TEMPERATURE,
+    // VAR_BATT_STATUS                ,
+    VAR_PANEL_VOLTAJE,
+    VAR_ULTRAVIOLE_INDEX,
+    VAR_BRIGHTNESS,
+    VAR_VOLATILE_ORGANIC_COMPUNDS,
+    // VAR_EPOCH
   };
 
   String getPayload();
   bool isReady();
 
-  void loop();
-  
-  DataManager(Scheduler *m_scheduller);
+  Task *tMain;
+  std::function<void()> mainFunction;
+
+  DataManager();
   virtual ~DataManager();
 
 private:
-
   typedef FactorySensorManager::TYPE_SENSOR_MANAGER ManagerType;
   typedef VARIABLE_SENSING_INTERVAL Interval;
-  typedef  SMOOTH_ALGORITHM Algorithm;
+  typedef SMOOTH_ALGORITHM Algorithm;
 
   FactorySensorManager *m_factoryManager;
   DataCleaner<long> *m_cleaner;
@@ -104,19 +104,19 @@ private:
   uint16_t intervals[MANAGER_SIZE];
   uint8_t algorithm[MANAGER_SIZE];
 
-
   Scheduler *m_dataScheduler;
   Task *m_tasks[MANAGER_SIZE];
+
 
   double rawValues[MANAGER_SIZE][SAMPLES];
 
   double variables[MANAGER_SIZE];
 
-  String json_array;
-
   void resetValues();
+  void loadConfig();
 
-  unsigned int lon;
-  unsigned int lat;
+  double lon;
+  double lat;
+  uint8_t refresh;
 };
 #endif // !defined(EA_542022A9_4749_4b6e_BA6F_80DFB9BB0B34__INCLUDED_)
