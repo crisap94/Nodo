d3d23c7f47d632749e936309f91dbacad4c8062e
Cristian Arrieta <cristianisaac94@gmail.com>
2019-05-22 00:50:31 -0500

    Implementations
    The System works

--- lib/Managers/DataManager.cpp
+++ lib/Managers/DataManager.cpp
@@ -10,7 +10,10 @@
 #include "ArduinoJson.h"
 
 #include "FS.h"
-DataManager::DataManager(Scheduler *m_scheduller)
+
+#include <Scheduller.h>
+
+DataManager::DataManager()
 
     : managers{ManagerType::AMBIENT_HUMIDITY,
                ManagerType::AMBIENT_TEMPERATURE,
@@ -55,102 +58,108 @@ DataManager::DataManager(Scheduler *m_scheduller)
                 //Algorithm::ALGORITHM_EPOCH
                 } {
 
-  Serial.println("DEBUG -> Starting Factory Manager");
+  Serial.println("\n\nDATA MANAGER -> Starting Factory Manager");
 
   m_factoryManager = new FactorySensorManager();
 
-  Serial.println("DEBUG -> Setting to NOT_SET all the Variables");
+  tMain =
+      new Task(TASK_SECOND, TASK_FOREVER, mainFunction, m_dataScheduler, false);
+
+  Serial.println("DATA MANAGER -> Setting to NOT_SET all the Variables");
   for (size_t manager = 0; manager < MANAGER_SIZE; manager++) {
     variables[manager] = NOT_SET;
   }
 
-  Serial.println("DEBUG -> Setting the matrix of Raw Values to NOT_SET");
+  Serial.println("DATA MANAGER -> Setting the matrix of Raw Values to NOT_SET");
   for (size_t managerData = 0; managerData < MANAGER_SIZE; managerData++) {
     for (size_t sample = 0; sample < SAMPLES; sample++) {
       rawValues[managerData][sample] = NOT_SET;
     }
   }
 
-  Serial.println("DEBUG -> Creating Scheduller");
-  m_dataScheduler = m_scheduller;
+  Serial.println("DATA MANAGER -> Copying Scheduller");
+  m_dataScheduler = &scheduller;
 
   for (size_t task = 0; task < MANAGER_SIZE; task++) {
-    Serial.printf("DEBUG -> Creating Task (%s)\n",
+    Serial.printf("DATA MANAGER -> Creating Task (%s)\n",
                   MANAGER_TYPE(managers[task]).c_str());
-    m_tasks[task] =
-        new Task(TASK_MILLISECOND * intervals[task], TASK_FOREVER,
-                 [this, task]() {
+    m_tasks[task] = new Task(
+        TASK_MILLISECOND * intervals[task], TASK_FOREVER,
+        [this, task]() {
 
-                   uint8_t dataset = task;
+          uint8_t dataset = task;
 
-                   /**
-                    * @brief Verify if the corresponding dataset of values has
-                    * any missing sample like NOT_SET, which mean the value has
-                    * not been taken.
-                    */
-                   for (size_t sample = 0; sample < SAMPLES; sample++) {
-                     if (rawValues[dataset][sample] == NOT_SET) {
-                       /**
-                        * @brief once the sample is found, create the
-                        * corresponding manager to get only 1 value
-                        * that corresponds to the dataset
-                        *
-                        */
+          /**
+           * @brief Verify if the corresponding dataset of values has
+           * any missing sample like NOT_SET, which mean the value has
+           * not been taken.
+           */
+          for (size_t sample = 0; sample < SAMPLES; sample++) {
+            if (rawValues[dataset][sample] == NOT_SET) {
+              /**
+               * @brief once the sample is found, create the
+               * corresponding manager to get only 1 value
+               * that corresponds to the dataset
+               *
+               */
 
-                       ManagerType type = (ManagerType)managers[task];
+              ManagerType type = (ManagerType)managers[task];
 
-                       SensorManager *manager;
-                       manager = m_factoryManager->createManager(type);
-                       this->rawValues[dataset][sample] = manager->getData();
+              SensorManager *manager;
+              manager = m_factoryManager->createManager(type);
+              this->rawValues[dataset][sample] = manager->getData();
 
-                       Serial.printf("Gettin Value from (%s) -> \t\t\t(%.1f)\n",
-                                     MANAGER_TYPE(managers[task]).c_str(),
-                                     this->rawValues[dataset][sample]);
+              Serial.printf("DATA MANAGER -> Gettin Value from (%s) -> \t\t\t(%.1f)\n",
+                            MANAGER_TYPE(managers[task]).c_str(),
+                            this->rawValues[dataset][sample]);
 
-                       delete manager;
+              delete manager;
 
-                       break;
+              break;
 
-                     } else if (sample == (SAMPLES - 1)) {
+            } else if (sample == (SAMPLES - 1)) {
 
-                       Serial.printf("\n\nDisabling task (%s)\n\n",
-                                     MANAGER_TYPE(managers[task]).c_str());
-                       /**
-                        * @brief if all the data are already gathered then
-                        * the task is disabled
-                        *
-                        */
-                       this->m_tasks[task]->disable();
+              Serial.printf("\n\nDATA MANAGER -> Disabling task (%s)\n\n",
+                            MANAGER_TYPE(managers[task]).c_str());
+              /**
+               * @brief if all the data are already gathered then
+               * the task is disabled
+               *
+               */
+              this->m_tasks[task]->disable();
 
-                       /**
-                        * @brief After
-                        *
-                        */
-                       m_cleaner = new DataCleaner<long>;
-                       m_cleaner->begin(algorithm[task], SAMPLES);
-                       for (size_t sample = 0; sample < SAMPLES; sample++) {
-                         m_cleaner->add(rawValues[task][sample]);
-                       }
-                       variables[task] = m_cleaner->get();
-                       Serial.printf(
-                           "Updating average value for (%s)(%.1f)\n\n",
-                           MANAGER_TYPE(managers[task]).c_str(),
-                           variables[task]);
-                       delete m_cleaner;
-                     }
-                   }
-                 },
-                 m_dataScheduler);
+              /**
+               * @brief After
+               *
+               */
+              m_cleaner = new DataCleaner<long>;
+              m_cleaner->begin(algorithm[task], SAMPLES);
+              for (size_t sample = 0; sample < SAMPLES; sample++) {
+                m_cleaner->add(rawValues[task][sample]);
+              }
+              variables[task] = m_cleaner->get();
+              Serial.printf("DATA MANAGER -> Updating average value for (%s)(%.1f)\n\n",
+                            MANAGER_TYPE(managers[task]).c_str(),
+                            variables[task]);
+              delete m_cleaner;
+            }
+          }
+        },
+        m_dataScheduler);
   }
 
-  for (size_t task = 0; task < MANAGER_SIZE; task++) {
-    // Enable all the Tasks previusly added to thescheduller
-    if (managers[task] == ManagerType::VOLATILE_ORGANIC_COMPUNDS) {
-      this->m_tasks[task]->enableDelayed(5000);
-    } else {
-      m_tasks[task]->enable();
+  mainFunction = [this]() {
+    loadConfig();
+    for (size_t task = 0; task < MANAGER_SIZE; task++) {
+      // Enable all the Tasks previusly added to thescheduller
+      if (managers[task] == ManagerType::VOLATILE_ORGANIC_COMPUNDS) {
+        this->m_tasks[task]->enableDelayed(5000);
+      } else {
+        m_tasks[task]->enable();
+      }
     }
-  }
+
+  };
 }
 
 DataManager::~DataManager() {}
@@ -176,8 +185,6 @@ String DataManager::getPayload() {
 
     delete m_bStatus;
     delete m_rtc;
-  data_gps["latitude"]            = this->lat;
-  data_gps["longitude"]           = this->lon;
 
     const size_t capacity = JSON_OBJECT_SIZE(15);
     DynamicJsonDocument doc(capacity);
@@ -192,11 +199,13 @@ String DataManager::getPayload() {
     doc["v"] = variables[VARIABLES::VAR_VOLATILE_ORGANIC_COMPUNDS];
     doc["pV"] = variables[VARIABLES::VAR_PANEL_VOLTAJE];
     doc["ti"] = EPOCH;
-    doc["la"] = "10.399080";
-    doc["lo"] = "-75.504142";
+    doc["la"] = lat;
+    doc["lo"] = lon;
     doc["bV"] = variables[VARIABLES::VAR_BATT_VOLTAJE];
     doc["bT"] = variables[VARIABLES::VAR_BATT_TEMPERATURE];
     doc["bS"] = battStatus;
+
+    String json_array;
 
     serializeJsonPretty(doc, json_array);
 
@@ -217,43 +226,37 @@ void DataManager::resetValues() {
   }
 }
 
-void DataManager::loop() { m_dataScheduler->execute(); }
+void DataManager::loadConfig() {
+  if (!SPIFFS.begin()) {
+    Serial.println(F("DATA MANAGER -> Error mounting File system"));
+  }
+  Serial.println(F("DATA MANAGER -> Success on mounting the File system"));
 
-void DataManager::loadConfig(){
-  File configFile = SPIFFS.open("/config.json", "r");
+  File file = SPIFFS.open("/config.json", "r");
 
-  size_t size = configFile.size();
-
-
-  // Asigne un buffer para almacenar el contenido del archivo.
-  std::unique_ptr<char[]> buf(new char[size]);
-
-  // No usamos String aquí porque la biblioteca ArduinoJson requiere la entrada
-  // buffer para ser mutable Si no usas ArduinoJson, también puedes
-  // use configFile.readString en su lugar.
-  configFile.readBytes(buf.get(), size);
-
-  const size_t capacity = JSON_OBJECT_SIZE(2) + JSON_OBJECT_SIZE(4) + JSON_OBJECT_SIZE(6) + 110;
+  const size_t capacity = 256;
   DynamicJsonDocument doc(capacity);
 
-  deserializeJson(doc, buf.get());
 
-  const char *zoneid = doc["zoneId"]; // "asdasdasdasdasdsadasd"
-  const char *topic = doc["topic"];   // "1234"
+  DeserializationError error = deserializeJson(doc, file);
 
-  this->zoneId = String(zoneid);
-  this->topic = String(topic);
+  //serializeJsonPretty(doc,Serial);
 
-   this->lat = doc["gps"]["lat"]; // 12.123123
-   this->lon = doc["gps"]["lon"]; // 23.123123
+  Serial.printf("\nJson Error: %s\n",error.c_str());
 
-  JsonObject time = doc["time"];
-  int time_hour = time["hour"];     // 23
-  int time_minute = time["minute"]; // 12
-  int time_second = time["second"]; // 12
-  int time_year = time["year"];     // 1234
-  int time_month = time["month"];   // 12
-  int time_day = time["day"];       // 12
+  lon = doc["latitud"];  // 10.12312312
+  lat = doc["longitud"]; // -90.12312312
+  refresh = doc["refreshWindow"];
 
-  this->m_now = new RtcDateTime(time_year,time_month,time_day,time_hour,time_minute,time_second);
+  Serial.println(F("DATA MANAGER -> Setting latitud and Longitud"));
+  Serial.print(F("Latitud:"));
+  Serial.println(lat,10);
+  Serial.print(F("Longitud:"));
+  Serial.println(lon,10);
+  Serial.print(F("Refresh Window:"));
+  Serial.println(refresh);
+
+  file.close();
+
+  SPIFFS.end();
 }
