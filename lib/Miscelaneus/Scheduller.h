157d112c87f003c03406028f409f8c71e8167698
Cristian Arrieta <cristianisaac94@gmail.com>
2019-05-21 22:37:46 -0500

    Global Scheduller
    Defined to control the whole system and all the tasks

---
+++ lib/Miscelaneus/Scheduller.h
@@ -0,0 +1,16 @@
+
+#ifndef _SCHEDULLER_
+#define _SCHEDULLER_
+
+#define _TASK_SLEEP_ON_IDLE_RUN
+#define _TASK_STATUS_REQUEST
+#define _TASK_STD_FUNCTION
+
+#include <TaskSchedulerDeclarations.h>
+
+// Let the runner object be a global, single instance shared between object
+// files.
+extern Scheduler scheduller;
+
+
+#endif
