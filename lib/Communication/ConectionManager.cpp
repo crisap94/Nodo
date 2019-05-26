48fd18e445c427863d44be0d602c75b62b8046c9
Cristian Arrieta <cristianisaac94@gmail.com>
2019-05-26 01:02:15 -0500

    Same as Gateway Debug Messages

--- lib/Communication/ConectionManager.cpp
+++ lib/Communication/ConectionManager.cpp
@@ -3,44 +3,19 @@
 
 ConectionManager::ConectionManager() {
 
-  Serial.println(F("\n\nCONECTION MANAGER -> Creating MESH Object"));
-  mesh = new painlessMesh();
-
   gatewayId = 0;
 
-  Serial.println(F("CONECTION MANAGER -> Creating MESH task"));
+  Serial.println(F("CONNECTION MANAGER -> Creating MESH task"));
 
   tMesh = new Task(TASK_SECOND, TASK_ONCE, initMesh, &scheduller);
 
-  mesh->setDebugMsgTypes(ERROR | MSG_TYPES | REMOTE | DEBUG | MESH_STATUS |
-                         CONNECTION | COMMUNICATION);
-
-  mesh->onReceive([this](const uint32_t &from, const String &msg) {
-    Serial.printf("Message: Received from %u msg=%s\n", from, msg.c_str());
-
-    const size_t capacity = JSON_OBJECT_SIZE(2) + 30;
-    DynamicJsonDocument doc(capacity);
-
-    deserializeJson(doc, msg);
-
-    if (doc.containsKey("gateway")) {
-      if (gatewayId == 0) {
-        if (String("mqtt").equals(doc["gateway"].as<String>())) {
-          // check for on: true or false
-          gatewayId = doc["nodeId"];
-          Serial.printf("CONECTION MANAGER -> ID Bridge Gateway Updated!\n");
-        }
-      }
-    }
-  });
-
-  Serial.println(F("CONECTION MANAGER -> Creating message Task"));
+  Serial.println(F("CONNECTION MANAGER -> Creating message Task"));
   tSendMessage =
       new Task(TASK_SECOND * 5, TASK_FOREVER, sendMessage, &scheduller);
 
   sendMessage = [this]() {
     if (gatewayId != 0) {
-      Serial.println(F("CONECTION MANAGER -> Gateway Connected"));
+      Serial.println(F("CONNECTION MANAGER -> Gateway Connected"));
       const size_t capacity = JSON_OBJECT_SIZE(2);
       DynamicJsonDocument doc(capacity);
 
@@ -48,25 +23,53 @@ ConectionManager::ConectionManager() {
 
       String str;
       serializeJson(doc, str);
-      Serial.println("CONECTION MANAGER -> Sending test Packet");
+      Serial.println("CONNECTION MANAGER -> Sending test Packet");
       // str = this->m_dataManager->getPayload();
       mesh->sendSingle(gatewayId, str);
       tSendMessage->disable();
 
     } else {
-      Serial.println(F("CONECTION MANAGER -> Gateway not visible yet"));
+      Serial.println(F("CONNECTION MANAGER -> Gateway not visible yet"));
     }
 
   };
 
   initMesh = [this]() {
-    
-    Serial.println(F("CONECTION MANAGER -> Init MESH "));
-    mesh->init(MESH_PREFIX, MESH_PASSWORD,&scheduller, MESH_PORT, WIFI_AP_STA,
+    Serial.println(F("\n\nCONNECTION MANAGER -> Creating MESH Object"));
+    mesh = new painlessMesh();
+
+    Serial.println(F("CONNECTION MANAGER -> Setting up Debug Messages"));
+    mesh->setDebugMsgTypes(ERROR | MSG_TYPES | REMOTE | DEBUG | MESH_STATUS |
+                           CONNECTION | COMMUNICATION);
+
+    Serial.println(F("CONNECTION MANAGER -> Init MESH "));
+    mesh->init(MESH_PREFIX, MESH_PASSWORD, &scheduller, MESH_PORT, WIFI_AP_STA,
                MESH_CHANNEL);
 
+    Serial.println(F("CONNECTION MANAGER -> Setting Up OnReceive Callback "));
+    mesh->onReceive([this](const uint32_t &from, const String &msg) {
+      Serial.printf("Message: Received from %u msg=%s\n", from, msg.c_str());
+
+      const size_t capacity = JSON_OBJECT_SIZE(2) + 30;
+      DynamicJsonDocument doc(capacity);
+
+      deserializeJson(doc, msg);
+
+      if (doc.containsKey("gateway")) {
+        if (gatewayId == 0) {
+          if (String("mqtt").equals(doc["gateway"].as<String>())) {
+            // check for on: true or false
+            gatewayId = doc["nodeId"];
+            Serial.printf("CONNECTION MANAGER -> ID Bridge Gateway Updated!\n");
+          }
+        }
+      }
+    });
+
+    Serial.println(F("CONNECTION MANAGER -> Enabling Send JSON Task"));
     tSendMessage->enable();
 
+    Serial.println(F("CONNECTION MANAGER -> Disabling Init Mesh Task"));
     tMesh->disable();
   };
 }
